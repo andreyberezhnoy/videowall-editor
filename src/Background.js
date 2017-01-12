9ee190e8822728a0a0e1fe4808a0998dba217456
Andrey Berezhnoy <devil.andrey@gmail.com>
2017-01-12 11:14:48 +0200

    Background component improvements

--- src/Background.js
+++ src/Background.js
@@ -5,45 +5,64 @@ export default class Background extends Component {
     super(props);
 
     this.state = {
-      url: 'https://www.gstatic.com/images/branding/googlelogo/2x/googlelogo_color_284x96dp.png'
+      image: 'https://www.gstatic.com/images/branding/googlelogo/2x/googlelogo_color_284x96dp.png',
+      color: '#fff',
+      showForm: false
     }
   }
 
   saveBackground = (event) => {
+    console.log(event);
+
     event.preventDefault();
 
-    const url = this.backgroundUrl.value;
-    this.setState({ url })
+    // Get color and image url
+    const image = this.image.value;
+    // const color = this.color.value;
 
-    if (!this.state.url) return false;
-
-    console.log(event);
+    // Set color and image url
+    this.setState({
+      showForm: false,
+      image
+    })
 
     this.renderBackground();
   }
 
-  renderInput = () => {
+  showForm = (event) => {
+    this.setState({
+      showForm: true
+    })
+
+    this.renderForm();
+  }
+
+  renderForm = () => {
+    if (!this.state.showForm) return false;
+
     return (
-      <form className="background-form" onSubmit={this.saveBackground}>
-        <input type="text" ref={(input => {this.backgroundUrl = input})} defaultValue={this.state.url} />
+      <form className="background-form" onSubmit={(e) => this.saveBackground(e)}>
+        <input type="text" ref={(input) => this.image = input} defaultValue={this.state.image} required />
       </form>
     )
   }
 
   renderBackground = () => {
-    if (!this.state.url) return false;
+    const backgroundStyles = {
+      backgroundImage: `url('${this.state.image}')`,
+      backgroundColor: this.state.color
+    }
 
     return (
-      <div className="background-image">
-        <img style={{width: '100%'}} src={this.state.url} alt=""/>
-      </div>
+      <div className="background-image" style={backgroundStyles}></div>
     )
   }
 
   render () {
     return (
       <div style={{width: '100%'}}>
-        {this.renderInput()}
+        <button className="background-edit" onClick={(e) => this.showForm(e)}>Edit</button>
+        {this.renderForm()}
         {this.renderBackground()}
       </div>
     );
